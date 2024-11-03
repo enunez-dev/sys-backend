88b8e3bc4fa52275f5a98c1a4565adee4cc37517
Natalia Alfaro <alfaro.natalia.lorena@gmail.com>
2024-11-03 07:47:46 -0400

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,15 @@ import { logger } from './utils/logger';
 
 // Crea la aplicación de Express
 const app = express();
-
+// Configure CORS options
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow requests from your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true, // If your frontend is sending cookies
+};
 // Configuración de middlewares
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(logger());
